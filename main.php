349bdf9c949165ec1519176109940353c70327ec
vikAyz9rY4Y7gT5 <KHALIDLOGI@GMAIL.COM>
2023-11-27 18:41:25 +0100

    enqueue class

--- main.php
+++ main.php
@@ -53,31 +53,18 @@ if (!class_exists('KHMYCLASS')) {
 
             // Hooks and Actions
             $this->regsiter_hooks();
-            //add_shortcode('display_form_values', array($this, 'display_form_values_shortcode'));
-            add_action('wp_enqueue_scripts', array($this, 'enqueue_form_values_css'));
-            add_action('admin_enqueue_scripts', array($this, 'enqueue_font_awesome'));
-
-            add_action('wp_enqueue_scripts', array($this, 'enqueue_custom_script'));
-            add_action('admin_enqueue_scripts', array($this, 'admin_styles'));
 
             // Activate if enabled
             if ($this->isdataenabled === '1') {
                 add_action('wpforms_process_entry_save', array($this, 'process_entry'), 10, 4);
             }
 
-
-            // Other actions
-            // add_filter('login_redirect', array($this, 'custom_login_redirect'), 10, 3);
             add_action('wp_login', array($this, 'redirect_to_saved_url'));
 
 
 
 
         }
-
-
-
-
 
 
         // Redirect users to the saved URL upon login
@@ -101,44 +88,8 @@ if (!class_exists('KHMYCLASS')) {
             load_plugin_textdomain(TABLESOME_DOMAIN, false, basename(dirname(__FILE__)) . '/languages');
         }
 
-        function enqueue_font_awesome()
-        {
-            wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css', array(), '5.15.3');
-        }
-
-        /**
-         * Enqueue CSS styles for the form values.
-         */
-        function enqueue_form_values_css()
-        {
 
 
-
-            // Enqueue your custom CSS.
-            wp_enqueue_style('form-values-style', plugin_dir_url(__FILE__) . 'assets/css/form-values.css');
-
-            // Enqueue Font Awesome from a CDN.
-            wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css', array(), '5.15.3');
-
-            // Enqueue jQuery UI stylesheet (optional).
-            wp_enqueue_style('jquery-ui-style', plugin_dir_url(__FILE__) . 'assets/css/jquery-ui.css');
-        }
-
-        /**
-         * Enqueue custom JavaScript script.
-         */
-        function enqueue_custom_script()
-        {
-            // Enqueue your custom JavaScript.
-            wp_enqueue_script('custom-script', plugin_dir_url(__FILE__) . 'assets/js/custom-script.js', array('jquery'), '1.0', true);
-
-            // Localize the script with custom variables for AJAX.
-            wp_localize_script('custom-script', 'custom_vars', array('ajax_url' => admin_url('admin-ajax.php')));
-
-            // Enqueue jQuery UI scripts (core and droppable) (optional).
-            wp_enqueue_script('jquery-ui-core', plugin_dir_url(__FILE__) . 'assets/js/jquery-ui-core', array('jquery'), '1.0', true);
-            wp_enqueue_script('jquery-ui-droppable');
-        }
         public function kh_wpfdb_activation()
         {
             if (!version_compare(PHP_VERSION, '5.4', '>=')) {
@@ -183,21 +134,11 @@ if (!class_exists('KHMYCLASS')) {
             include_once KHFORM_PATH . 'Inc/AjaxClass.php';
             include_once KHFORM_PATH . 'Inc/KHwidget.php';
             include_once KHFORM_PATH . 'Inc/display_form_values_shortcode.php';
+            include_once KHFORM_PATH . 'Inc/EnqueueClass.php';
 
         }
 
-        /**
-         * Styles for Dashboard
-         *
-         * @return void
-         */
-        function admin_styles()
-        {
-            wp_enqueue_style('admin_style', plugin_dir_url(__FILE__) . 'assets/css/admin.css');
-            wp_enqueue_style('admin_style', plugin_dir_url(__FILE__) . 'assets/css/bootstrap.min.css');
 
-
-        }
 
 
         /**
@@ -393,10 +334,6 @@ if (!class_exists('KHMYCLASS')) {
             exit;
 
         }
-
-
-
-
 
 
         /**
